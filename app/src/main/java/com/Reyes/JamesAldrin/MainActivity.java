b2f9af1ef30758e77ff3d4681d994319d1224d55
James <jamesaldrin.reyes.iics@ust.edu.ph>
2018-09-10 11:29:17 +0800

    Initial commit

---
+++ app/src/main/java/com/Reyes/JamesAldrin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Reyes.JamesAldrin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Styles & Material Design");
+        setContentView(R.layout.constraint_layout);
+    }
+}
